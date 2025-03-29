b63e748f234bc48a6c5d2cad76751355380f0345
gitanshugupta7 <gitanshugupta77@gmail.com>
2025-03-30 00:42:36 +0530

    added all pages

--- gipl_app/urls.py
+++ gipl_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('products/<slug:category_slug>/', views.category_products, name='category_detail'),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
     path('contact/', views.contact, name='contact'),
+    path('global-presence/', views.global_presence, name='global_presence')
     
     # Additional URL patterns can be added here
 ]
