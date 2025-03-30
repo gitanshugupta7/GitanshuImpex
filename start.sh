c9212d46c0f2fac18d6d89c2c4a2199dcbf91716
Adrish1999 <adrishbose198@gmail.com>
2025-03-30 09:02:23 +0000

    changes

--- start.sh
+++ start.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Activate virtual environment
+source ../venv/bin/activate
+
+# Navigate to project folder
+cd /home/gitanshuimpex/gitanshuimpex
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
-gunicorn gipl_website.wsgi:application --bind 0.0.0.0:$PORT
+
+# Start Gunicorn server (running on port 8001 for internal use)
+exec gunicorn gipl_website.wsgi:application \
+  --bind 127.0.0.1:8001 \
+  --workers 3 \
+  --timeout 120 \
+  --log-level info \
+  --log-file logs/gunicorn.log
+
+
