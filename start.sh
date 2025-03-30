f56d29de0f0637d2b409fbe3eb64979146e570d1
gitanshugupta7 <gitanshugupta77@gmail.com>
2025-03-30 14:31:36 +0530

    changed start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
+# Activate virtual environment
+source /home/gitanshuimpex/venv/bin/activate
+
+# Load environment variables
+export $(cat /home/gitanshuimpex/gitanshuimpex/.env | xargs)
+
+# Navigate to the project directory
+cd /home/gitanshuimpex/gitanshuimpex
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
-gunicorn gipl_website.wsgi:application --bind 0.0.0.0:$PORT
+
+# Start Gunicorn
+exec gunicorn gipl_website.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --timeout 120
