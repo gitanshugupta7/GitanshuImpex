33e3d5c99ff13da7ad69d11dd9fad479c83b3c5a
gitanshugupta77 <gitanshugupta77@users.noreply.github.com>
2026-03-14 15:48:40 +0000

    preparing to pull

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gitanshuimpex/gitanshuimpex/venv"
+set -gx VIRTUAL_ENV "/var/www/gitanshuimpex/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
