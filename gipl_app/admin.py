dc8987aafb16ee596d21a58a72e1692ed5ebd97c
gitanshugupta7 <gitanshugupta77@gmail.com>
2026-03-15 12:26:58 +0530

    fixed admin.py

--- gipl_app/admin.py
+++ gipl_app/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 from .models import *
+
 admin.site.register(Testimonial)
 admin.site.register(Inquiry)
+
+# 1. New: Register the Subdomain Group to manage high-level prefixes
+@admin.register(Subdomain)
+class SubdomainGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prefix', 'meta_title')
+    search_fields = ('name', 'prefix')
+
 @admin.register(CarouselItem)
 class CarouselItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'cta_text', 'cta_link')
 
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
-    # Added subdomain_prefix so you can see it in the main table view
-    list_display = ('name', 'subdomain_prefix', 'slug') 
+    # 2. Change: Display the 'subdomain' foreign key instead of a text prefix
+    list_display = ('name', 'subdomain', 'slug') 
+    list_filter = ('subdomain',) # Filter by subdomain to stay organized
     prepopulated_fields = {"slug": ("name",)}
 
 @admin.register(Product)
